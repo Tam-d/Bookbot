b8983ce2b9fbe3eec6160e3f194b1e46f33c1eaa
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-08 16:53:05 +0000

    CH2L4: count words

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ def get_book_text(file_path):
 
     return file_contents
 
+def get_book_word_count(book_text):
+    return len(book_text.split())
+    
+
 
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    book_text = get_book_text("./books/frankenstein.txt")
+    book_word_count = get_book_word_count(book_text)
+
+    print(f"Found {book_word_count} total words")
 
 
 main()
