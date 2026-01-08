8cd80a87a5961be994720b449e0ae149be9bb318
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-08 19:27:37 +0000

    CH3L2: add argument handling, add new books

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_book_word_count, get_book_char_count, sort_char_dictionary
 from report import print_report
+import sys
 
 
 def get_book_text(file_path):
@@ -12,7 +13,14 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+
+    if(len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+
+    book_path = sys.argv[1]
+
+    book_text = get_book_text(book_path)
     book_word_count = get_book_word_count(book_text)
     book_char_count = get_book_char_count(book_text)
     sorted_chars = sort_char_dictionary(book_char_count)
