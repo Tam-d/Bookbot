15229bfeabd1ae359bed7f7a554a0e74f99dc479
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-08 19:04:00 +0000

    CH3L1: print a report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_word_count, get_book_char_count
+from stats import get_book_word_count, get_book_char_count, sort_char_dictionary
 
 
 def get_book_text(file_path):
@@ -10,15 +10,38 @@ def get_book_text(file_path):
 
     return file_contents
 
+
+def print_word_count(word_count):
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+
+
+def print_char_dict(char_dict):
+    print(f"{char_dict["char"]}: {char_dict["num"]}")
+
+def print_char_dict_list(char_dict_list):
+    for item in char_dict_list:
+        print_char_dict(item)
+
+def print_char_count(char_dict_list):
+    print("--------- Character Count -------")
+    print_char_dict_list(char_dict_list)
+
+def print_report(word_count, char_dict_list):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+
+    print_word_count(word_count)
+    print_char_count(char_dict_list)
+
+    print("============= END ===============")
+
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
     book_word_count = get_book_word_count(book_text)
-
-    print(f"Found {book_word_count} total words")
-
     book_char_count = get_book_char_count(book_text)
+    sorted_chars = sort_char_dictionary(book_char_count)
 
-    print(book_char_count)
-
+    print_report(book_word_count, sorted_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,24 @@ def get_book_char_count(book_text):
             char_dictionary[curr_character] = 1
     
     return char_dictionary
+
+def sort_item(item):
+    return item["num"]
+
+def sort_char_dictionary(char_dict):
+
+    char_list = []
+
+    for key in char_dict:
+
+        per_char_dict = {
+            "char" : key,
+            "num" : char_dict[key]
+        }
+
+        char_list.append(per_char_dict)
+
+    char_list.sort(key=sort_item, reverse=True)
+
+    return char_list
+    
